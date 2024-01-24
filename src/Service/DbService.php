c815b413b763b4212a8c3819acb26ce130752db1
Muhammad Rao <muhammad.rao@savyour.com>
2024-01-24 23:44:11 +0500

    DB connection service initiated

--- src/Service/DbService.php
+++ src/Service/DbService.php
@@ -4,5 +4,40 @@ namespace SalesDashboard\Service;
 
 class DbService
 {
+    private $host = 'localhost';
+    private $dbname = 'bookshop';
+    private $username = 'root';
+    private $password = 'muhammad';
+
+    private $connection;
+
+    public function __construct()
+    {
+        $this->connect();
+    }
+
+    private function connect()
+    {
+        $dsn = "mysql:host={$this->host};dbname={$this->dbname}";
+
+        try {
+            $this->connection = new \PDO($dsn, $this->username, $this->password);
+            // Set PDO to throw exceptions on error
+            $this->connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        } catch (\PDOException $e) {
+            // Handle connection errors
+            die("Connection failed: " . $e->getMessage());
+        }
+    }
+
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+
+    public function closeConnection()
+    {
+        $this->connection = null;
+    }
 
 }
